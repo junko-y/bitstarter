f81bd40f74841bc29e56902861e751519b60a34e
Ubuntu <ubuntu@ip-172-31-25-163.us-west-2.compute.internal>
2013-07-12 05:39:59 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var buf = new Buffer(fs.readFileSync('index.html'));
   response.send(bug.toString('ascii',0,bug.length));
 });
